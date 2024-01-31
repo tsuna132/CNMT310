9786d0c58caadc5705a5cc3b804bece034f7cd2a
Sunar <tsuna132@uwsp.edu>
2024-01-31 12:47:51 -0600

    day 2

---
+++ action.php
@@ -0,0 +1,5 @@
+<?php
+var_dump($_POST["pass1"] == $_POST['pass2'){
+    print "passwprds match";
+}
+exit;
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+print "<!DOCTYPE html>". PHP_EOL;
+print "<html>" . PHP_EOL;
+print "<head>" . PHP_EOL;
+print "\t<title>PHP Page</title>" . PHP_EOL; 
+print "</head>" . PHP_EOL;
+print "<body>" . PHP_EOL;
+print "<h1>This is a heading</h1>" . PHP_EOL;
+
+print "\t<h1>Time is </h1>" .date('c')."</c>";
+
+print "</body>". PHP_EOL;
+print "</html>". PHP_EOL;
